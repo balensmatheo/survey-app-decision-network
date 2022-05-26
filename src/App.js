47a41e24679110309f0eaa08c7f54414f1b97691
Balens <matheobalens@gmail.com>
2022-05-26 15:30:34 +0200

    Ajout du logo

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import awsExports from "./aws-exports";
 import {
     Button, Flex,
     Heading,
-    Icon, Image, Text,
+    Icon,
 } from "@aws-amplify/ui-react";
 
 import Formations from "./components/Formations/Formations"
 import Formulaires from "./components/Formulaires/Formulaires";
 import Success from "./components/Success/Success";
 import SignIn from "./components/SignIn/SignIn";
-import logo from "./assets/Decision_Network.png"
+import logo from "./assets/logo_dn_png.png"
 
 
 import {useEffect, useState} from "react";
@@ -68,29 +68,31 @@ function App() {
     return (
         <main>
             <nav className={"navbar"}>
-                <Heading flex={"1 1 auto"} fontWeight={500} color={"whitesmoke"} level={1}>
+                <Flex direction={"row"} flex={"1 1 auto"}>
                     <Link className={"nav-heading"} to={'*'}>
-                        <Flex direction={"row"} alignItems={"center"} justifyContent={"flex-start"}>
-                            <Image className={"logo"} height={"70px"} width={"70px"} alt={"Decision Network Logo"} src={logo}/>
-                            <Heading fontFamily={"Roboto"} fontWeight={500} level={1} marginRight={"0.5em"} color={"white"}>Espace Formations</Heading>
+                        <Flex flex={"1 1 auto"} direction={"row"} alignItems={"center"} justifyContent={"flex-start"}>
+                            <img src={logo} alt={"logo"} className={"logo"}/>
+                            <Heading fontSize={"calc(10pt + 3.3vmin)"} fontFamily={"Roboto"} fontWeight={500} level={1} color={"white"}>Espace Formations</Heading>
                         </Flex>
                     </Link>
-                </Heading>
-                <Link className={"link-accueil"} to={"*"}>
-                    <Icon marginRight={"1em"} marginTop={"0.1em"} fontSize={"26pt"} viewBox={{width: 48, height: 48}}
-                          color={"white"} ariaLabel={"accueil"}
-                          pathData={"M8.25 41.75V18.1L24.1 6.25L39.8 18.1V41.75H28.25V27.7H19.75V41.75Z"}></Icon>
-                </Link>
-                {
-                    loggedIn ?
-                        <Button size={"small"} fontFamily={"Roboto"} border={"none"} backgroundColor={"#ffaeae"} marginRight={"1em"}
-                                onClick={signOut}>Déconnexion</Button>
-                        :
-                        <Link className={"signInLink"} to={"signIn"}>
-                            <Button fontFamily={"Roboto"} size={"small"} border={"none"} backgroundColor={"#aee3ff"} marginRight={"1em"}>Connexion</Button>
-                        </Link>
-                }
+                </Flex>
 
+                <Flex direction={"row"} justifyContent={"flex-start"} alignItems={"flex-start"}>
+                    <Link className={"link-accueil"} to={"*"}>
+                        <Icon marginRight={"1em"} marginTop={"0.1em"} fontSize={"26pt"} viewBox={{width: 48, height: 48}}
+                              color={"white"} ariaLabel={"accueil"}
+                              pathData={"M8.25 41.75V18.1L24.1 6.25L39.8 18.1V41.75H28.25V27.7H19.75V41.75Z"}></Icon>
+                    </Link>
+                    {
+                        loggedIn ?
+                            <Button size={"small"} fontFamily={"Roboto"} border={"none"} backgroundColor={"#ffaeae"}
+                                    onClick={signOut}>Déconnexion</Button>
+                            :
+                            <Link className={"signInLink"} to={"signIn"}>
+                                <Button fontFamily={"Roboto"} size={"small"} border={"none"} backgroundColor={"#aee3ff"} marginRight={"1em"}>Connexion</Button>
+                            </Link>
+                    }
+                </Flex>
             </nav>
             <Routes>
                 <Route element={<Formations user={user}/>} path={"*"} component={Formations}/>
